07b64db28be1f2a2a29df1496e820a5d1cdada2a
Карина Дроздова <K.Drozd33@yandex.ru>
2021-04-19 13:12:42 +0300

    ..

--- Book/Class1.cs
+++ Book/Class1.cs
@@ -29,7 +29,7 @@ namespace Book
         {
             for (int i=0; i < k - 1; i++)
             {
-                for (int j = 0; j < k - i; j++)
+                for (int j = 0; j < k - i-1; j++)
                 {
                     if (String.Compare(books[j].Ganr, books[j + 1].Ganr)<0)
                     {
@@ -37,10 +37,42 @@ namespace Book
                         books[j] = books[j + 1];
                         books[j + 1] = b1;
                     }
+                    else 
+                    {
+                        if (String.Compare(books[j].Avtor, books[j + 1].Avtor) < 0 && String.Compare(books[j].Ganr, books[j + 1].Ganr) == 0)
+                        {
+                            Book b1 = books[j];
+                            books[j] = books[j + 1];
+                            books[j + 1] = b1;
+                        }
+                        else
+                        {
+                            if (String.Compare(books[j].Name, books[j + 1].Name) < 0 
+                                && String.Compare(books[j].Ganr, books[j + 1].Ganr)== 0 && String.Compare(books[j].Avtor, books[j + 1].Avtor) ==0)
+                            {
+                                Book b1 = books[j];
+                                books[j] = books[j + 1];
+                                books[j + 1] = b1;
+                            }
+                        }
+                    }
+                  
                 }
             }
         }
 
+        public void seft_to_file()
+        {
+            using (StreamWriter sw = new StreamWriter("test.txt", false, System.Text.Encoding.Default))
+            {
+                for (int i =0; i<k; i++)
+                {
+                    sw.WriteLine(books[i].Avtor);
+                    sw.WriteLine(books[i].Name);
+                    sw.WriteLine(books[i].Ganr);
+                }
+            }
+        }
 
     }
 }
--- Book/Program.cs
+++ Book/Program.cs
@@ -8,7 +8,26 @@ namespace Book
         {
             Console.WriteLine("Размер массива:");
 
+            int n = Convert.ToInt32(Console.ReadLine());
 
+            Class1 librarary = new Class1(n);
+            for(int  i = 0; i < n; i++)
+            {
+                Console.WriteLine("заполнение записи №" + (i + 1).ToString());
+                Console.WriteLine("Размер массива:");//avtor
+                string s1 = Console.ReadLine();
+                Console.WriteLine("Размер массива:");
+                string s2 = Console.ReadLine();
+                Console.WriteLine("Размер массива:");
+                string s3 = Console.ReadLine();
+                librarary.Add(s1, s2, s3);
+            }
+
+            librarary.Sort();
+            librarary.seft_to_file();
+
+
+            Console.ReadKey();
 
         }
     }
