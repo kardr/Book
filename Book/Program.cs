f2af442c2756fc44646a5757aac8abb7636ec2f0
Карина Дроздова <K.Drozd33@yandex.ru>
2021-04-19 14:50:33 +0300

    .

--- Book/Program.cs
+++ Book/Program.cs
@@ -6,29 +6,44 @@ namespace Book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Размер массива:");
-
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            Class1 librarary = new Class1(n);
-            for(int  i = 0; i < n; i++)
+            try
             {
-                Console.WriteLine("Заполнение записи №" + (i + 1).ToString());
-                Console.WriteLine("Наименование:");
-                string s1 = Console.ReadLine();
-                Console.WriteLine("Автор:");
-                string s2 = Console.ReadLine();
-                Console.WriteLine("Жанр:");
-                string s3 = Console.ReadLine();
-                librarary.Add(s1, s2, s3);
+                Console.WriteLine("Размер массива:");
+
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n == 0)
+                {
+                    Console.WriteLine("Введите число больше 0!", "Ошибка");
+                }
+               else if (n < 0)
+                {
+                    Console.WriteLine("Введите число больше 0!", "Ошибка");
+                }
+                Class1 librarary = new Class1(n);
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Заполнение записи №" + (i + 1).ToString());
+                    Console.WriteLine("Наименование:");
+                    string s1 = Console.ReadLine();
+                    Console.WriteLine("Автор:");
+                    string s2 = Console.ReadLine();
+                    Console.WriteLine("Жанр:");
+                    string s3 = Console.ReadLine();
+                    librarary.Add(s1, s2, s3);
+                }
+
+                librarary.Sort();
+                librarary.seft_to_file();
+
+
+                Console.ReadKey();
             }
-
-            librarary.Sort();
-            librarary.seft_to_file();
-
-
-            Console.ReadKey();
-
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите число!", "Ошибка");
+            }
         }
     }
 }
