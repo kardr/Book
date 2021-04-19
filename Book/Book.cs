ec0acd6bee87ea855d6a727e1bd83fbbc1cdcb06
Карина Дроздова <K.Drozd33@yandex.ru>
2021-04-19 12:23:52 +0300

    Добавьте файлы проекта.

---
+++ Book/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book
+{
+    class Book
+    {
+        public string Name;
+        public string Avtor;
+        public string Ganr;
+        public Book(string nName, string nAvtor, string nGanr)
+        {
+            Name = nName;
+            Avtor = nAvtor;
+            Ganr = nGanr;
+        }
+
+
+
+    }
+}
---
+++ Book/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
